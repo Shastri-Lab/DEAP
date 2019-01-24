525647f13c1c47ccccaa102ff08bcf6e1584180f
Viraj Bangari <Viraj3f@gmail.com>
2019-01-23 19:28:07 -0500

    Start repo

---
+++ helpers.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def bisect_min(array, val):
+    array = np.asarray(array)
+    index = np.searchsorted(array, val, side='left')
+
+    if index == array.size:
+        return array.size - 1
+    elif index == 0:
+        return 0
+
+    if index == array.size - 1:
+        offsetArray = np.array([
+            array[index - 1],
+            array[index]])
+    else:
+        offsetArray = np.array([
+            array[index - 1],
+            array[index],
+            array[index + 1]])
+
+    return np.argmin(np.abs(val - offsetArray)) - 1 + index
